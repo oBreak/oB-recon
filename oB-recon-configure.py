81176b588d7525db6f208a975b452628a415974d
oBreak <obreakemail@gmail.com>
2018-07-17 14:13:52 -0400

    Commented on usage. Better explanation of nmap usage within program.

--- oB-recon-configure.py
+++ oB-recon-configure.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 
-'''
+"""
 Writes a configuration file for the oB-recon program. This controls which
 tests are run.
 
-Scan1   = Example
+Scan1   = Scan 1 target is set up to do a "quick scan" from nmap on target x.
+          The command line is 'nmap -T4 -F target'
 
+          In order to do a more intense scan, the options would be nmap -T4 -A -v <ipaddress>,
+          this is not a configurable toggle within the oB-recon program.
 
-
-Scan2   = NOT ADDED YET --- WebLogic RCE (Remote Code Execution) - from https://github.com/frohoff/ysoserial
-    This requires input of 'ip:port command' as arguments.
-
-
+Scan2   = Example
 
 Scan3   = ARIN.net lookup for the IP in question.
-
-
-
-'''
+"""
 
 # Import
 import configparser
@@ -28,14 +24,13 @@ import configparser
 # Functions
 def writeConf():
     conf = configparser.ConfigParser()
-    conf['credentials-example']         = {'user1': 'example', 'pass1': 'password', 'user2': 'admin', 'pass2': 'root'}
-    conf['connectionstring-example']    = {'servername': 'server', 'port': '23'}
+    #conf['credentials-example']         = {'user1': 'example', 'pass1': 'password', 'user2': 'admin', 'pass2': 'root'}
+    #conf['connectionstring-example']    = {'servername': 'server', 'port': '23'}
     conf['scans']                       = {'scan1': 'False',\
                                            'scan2': 'False',\
                                            'scan3': 'True'}
-
     with open ('conf.ini', 'w') as configfile:
         conf.write(configfile)
     return
-
+# Main
 writeConf()
